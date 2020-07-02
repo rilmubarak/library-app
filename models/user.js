a47ecded3893fa5017ae259694f9c3e1e033c5f9
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2020-07-02 16:34:15 +0700

    many to many

--- models/user.js
+++ models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    get fullName(){
+      return `${this.first_name} ${this.last_name}`
+    }
     static associate(models) {
       // define association here
       User.belongsToMany(models.Book, { through: "UserBooks" });
