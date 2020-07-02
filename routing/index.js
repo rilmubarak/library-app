84ddb07bac21c896686a84ed4619abd58da53141
mubaraqhere <mubaraqhere@gmail.com>
2020-07-02 22:42:53 +0700

    index routing changed

--- routing/index.js
+++ routing/index.js
@@ -1,14 +1,27 @@
 const router = require('express').Router()
 const adminRouter = require('./adminRouter')
-const bookRouter = require('./bookRouter')
+// const bookRouter = require('./bookRouter')
 const userRouter= require('./userRouter')
 const HomeController = require('../controllers/HomeController')
 
 router.get('/', HomeController.view)
 
-router.use('/admins', adminRouter)
-router.use('/books', bookRouter)
-router.use('/users', userRouter)
+router.get('/signup', HomeController.showSignUpUser)
+router.post('/signup', HomeController.signUpProcess)
+
+router.get('/signin', HomeController.showSignInUser)
+router.post('/signin', HomeController.signInProcess)
+
+router.get('/signin-admin', HomeController.showSignInAdmin)
+router.post('/signin-admin', HomeController.signInAdminProcess)
+
+router.get('/signout', HomeController.signOut)
+
+router.use('/admin', adminRouter)
+router.use('/user', userRouter)
+
+router.get('/unauthorized', HomeController.unauthorized)
+router.get('/*', HomeController.notFound)
 
 
 module.exports = router
